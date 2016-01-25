a97536a299b924a10b603ad91365e79cea0df6ce
Mollie Bootsma and Ronando Long <student@epicodus.com>
2016-01-25 09:20:28 -0800

    It will take in user input for first and last name and output, to same page, contact info.

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,35 @@
-// Business Logic
-// var helloWorld = function(){
-//   return false;
-//
-//   For loops and shit go here
-//   If/else like a madman here
-//
-//  return shit like a mo-fo here
-//
-//   ...whatchyu lookin at bitch? Green those specs and git back to work!
-//  Time fo yo Interface Logic, then suckface when it's all green.
-// };
+function Contact(firstName,lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.address = [];
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
+
+$(document).ready(function() {
+  $("form.ggg").submit(function(event) {
 
 
+    var inputtedFirstName = $("input.firstName").val();
+
+    var inputtedLastName = $("input.lastName").val();
+    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+
+    $("input.firstName").val();
+    $("input.lastName").val();
+
+    $(".contact").last().click(function() {
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.fullName());
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+    });
 
 
-
-
-
-
-// User Interface Logic
-// $(document).ready(function() {
-//   $("form#IDselector").submit(function(event) {
-//     // variables and shit go here, bitch!
-//
-//     // link your muthafuckin' variables to yo bad-ass bidness logic function, Yo!
-//
-//     // Make shit happen here...BITCH!
-//
-//     // Muthafuckin' variables connect to your output down here hommie.
-//
-//     // Show me some money, hunny, and reveal that hiddin fine shit you got stashed.
-//
-//     // It's all good baby.'
-//     event.preventDefault();
-//   });
-// });
+    event.preventDefault();
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,14 @@
-// describe('helloWorld', function(){
-//   it("is going to be false", function(){
-//     expect(helloWorld()).to.equal(false);
-//   });
-// });
+describe('Contact', function() {
+  it("creates a new contact with the given information", function() {
+    var testContact = new Contact("Mollie", "Bootsma");
+    expect(testContact.firstName).to.equal("Mollie");
+    expect(testContact.lastName).to.equal("Bootsma");
+    expect(testContact.address).to.eql([]);
+  });
+
+  it("adds the fullName method to all contacts", function() {
+    var testContact = new Contact("Ronando","Long");
+    expect(testContact.fullName()).to.equal("Ronando Long");
+  });
+
+});
